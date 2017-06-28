b4770bbfba3f6263f2a4a606c1537e4493665b0f
Integration <Integration>
2017-06-28 20:23:11 +0800

    output uppercase

--- polyalphabetic_substitution_ciphers.c
+++ polyalphabetic_substitution_ciphers.c
@@ -1,21 +1,59 @@
-// Work with lowercase plaintext and pad only
+// Work with lowercase plaintext and pad input only
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-char* en(char* inp, char* pad) {
+char* to_upper_case(char* inp) {
+	int len = strlen(inp);
+	char* ret = calloc(len + 1, sizeof(char));
+	
+	for (int i = 0; i < len; i++)
+		ret[i] = toupper(inp[i]);
+	return ret;
+}
+
+char* en(char* inp, char* _pad) {
 	int i = 0, k = 0;
 	char* ret = malloc(strlen(inp) + 1);
 	
-	for (; inp[i]; i++) {
-		if (inp[i] == ' ') {
+	char* plaintext = to_upper_case(inp);
+	char* pad = to_upper_case(_pad);
+	
+	for (; plaintext[i]; i++) {
+		if (plaintext[i] == ' ') {
 			ret[i] = ' ';
 			continue;
 		}
 		
-		int a, b, c;
-		c = (inp[i] - 'a') + (pad[k] - 'a') + 1;
-		c %= 26;
-		c += 'a';
+		int c;
+		c = (plaintext[i] - 'A') + (pad[k] - 'A') + 1;
+		c = (c % 26) + 'A';		// 'a' for lowercase output
+		
+		ret[i] = c;
+		k = (k + 1) % strlen(pad);
+	}
+	return ret;
+}
+
+char* de(char* inp, char* _pad) {
+	int i = 0, k = 0;
+	char* ret = malloc(strlen(inp) + 1);
+	
+	char* plaintext = to_upper_case(inp);
+	char* pad = to_upper_case(_pad);
+	
+	for (; plaintext[i]; i++) {
+		if (plaintext[i] == ' ') {
+			ret[i] = ' ';
+			continue;
+		}
+		
+		int c;
+		c = (plaintext[i] - 'A') - (pad[k] - 'A') - 1;
+		if (c < 0) c += 26;		// **CRITICAL**
+		c = (c % 26) + 'A';		// 'a' for lowercase output
 		
 		ret[i] = c;
 		k = (k + 1) % strlen(pad);
@@ -25,11 +63,15 @@ char* en(char* inp, char* pad) {
 
 int main(void) {
 	char* p = "apple banana cipher dounut elephant flint";
-	char* pad = "qwerty";
+	char* pad = "QWERTY";
 	
-	printf("\nbefore = %s\nafter  = %s\n", p, en(p, pad));
+	printf("before = %s\nafter  = %s\n", p, en(p, pad));
 	// before = apple banana cipher dounut elephant flint
 	// after  = rmudy arkffu bzmmwl cfrsmn dcbuzumk cqahs
+	
+	printf("after  = %s\n", de(en(p, pad), pad));
+	
+	//printf("\nbefore:|%s|\nafter :|%s|\n", p, to_upper_case(p));
 	
 	return 0;
 }
